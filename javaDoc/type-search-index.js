4c1c8e6b9d2f3191c35e8e2e363d32237fdce3a6
Dyfog <mariolaurie2003@gmail.com>
2023-06-28 19:15:48 -0400

    creacion javadoc
    se creo el javadoc pero sin contenidos reales, solo para comprobar su funcionamiento

---
+++ javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Calculo"},{"p":"org.example","l":"CotizacionManicura"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"ManejoArchivo"},{"p":"org.example","l":"Servicio"},{"p":"org.example","l":"Util"}];updateSearchResults();
